a0e96add329278f828edd427985546cb0c2db28f
noahdotpy <noahdotpy@proton.me>
2024-07-09 15:48:22 +1000

    fix: add a console.log() to try and fix image-pinned-etcs

--- modules/image-pinned-etcs/image-pinned-etcs.sh
+++ modules/image-pinned-etcs/image-pinned-etcs.sh
@@ -22,16 +22,16 @@ if [[ ${#ADD_FILES[@]} -gt 0 ]]; then
 		DIRS_TO_CREATE=($(fd --type directory --base-directory $CONFIG_DIRECTORY/image-pinned-etcs/$entry | xargs))
 		FILES_TO_LINK=($(fd --type file --base-directory $CONFIG_DIRECTORY/image-pinned-etcs/$entry | xargs))
 
+		echo "files to link: $FILES_TO_LINK"
+
 		for dir in "${DIRS_TO_CREATE[@]}"; do
 			mkdir -p /usr/etc/$dir
 		done
 
 		for file in "${FILES_TO_LINK[@]}"; do
-			if [ -L "$file" ]; then
-				continue
-			fi
 			ln -s /usr/share/ublue-os/image-pinned-etcs/$file /usr/etc/$file
-
+			# TODO: for some reason there is already aurora-fastfetch.sh and its trying to make it again
+			# https://github.com/noahdotpy/myublue/actions/runs/9842677770/job/27172121481#step:2:1856
 		done
 	done
 fi
