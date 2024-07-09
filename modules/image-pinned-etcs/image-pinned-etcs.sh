bf51dce9e38f1ca6abb108b83e60c0985053a8f9
noahdotpy <noahdotpy@proton.me>
2024-07-09 17:34:09 +1000

    chore: remove lines meant only for testing

--- modules/image-pinned-etcs/image-pinned-etcs.sh
+++ modules/image-pinned-etcs/image-pinned-etcs.sh
@@ -10,25 +10,17 @@ mkdir -p /usr/share/ublue-os/image-pinned-etcs/
 if [[ ${#ADD_FILES[@]} -gt 0 ]]; then
 	cd "$CONFIG_DIRECTORY/image-pinned-etcs"
 
-	echo "$CONFIG_DIRECTORY/image-pinned-etcs"
-	tree . -f
-
 	echo "Adding files to image-pinned-etcs"
 	for entry in "${ADD_FILES[@]}"; do
-		echo "$CONFIG_DIRECTORY/image-pinned-etcs/$entry"
-		tree $CONFIG_DIRECTORY/image-pinned-etcs/$entry -f
 		if [ ! -e "$CONFIG_DIRECTORY/image-pinned-etcs/$entry" ]; then
 			echo "Entry $entry Does Not Exist in $CONFIG_DIRECTORY/image-pinned-etcs"
 			exit 1
 		fi
 
 		echo "Copying $entry to /usr/share/ublue-os/image-pinned-etcs"
-		echo "config-directory: $CONFIG_DIRECTORY"
 		cp -rf $CONFIG_DIRECTORY/image-pinned-etcs/$entry/* /usr/share/ublue-os/image-pinned-etcs/
 		DIRS_TO_CREATE=($(fd --type directory --base-directory $CONFIG_DIRECTORY/image-pinned-etcs/$entry | xargs))
 		FILES_TO_LINK=($(fd --type file --base-directory $CONFIG_DIRECTORY/image-pinned-etcs/$entry | xargs))
-
-		echo "files to link: $FILES_TO_LINK"
 
 		for dir in "${DIRS_TO_CREATE[@]}"; do
 			mkdir -p /usr/etc/$dir
@@ -39,10 +31,8 @@ if [[ ${#ADD_FILES[@]} -gt 0 ]]; then
 			if [ -e /usr/etc/$file ]; then
 				rm /usr/etc/$file
 			fi
-			echo "linking this thing /usr/share/ublue-os/image-pinned-etcs/$file /usr/etc/$file"
+			echo "Creating symlink at /usr/etc/$file that points to /usr/share/ublue-os/image-pinned-etcs/$file"
 			ln -s /usr/share/ublue-os/image-pinned-etcs/$file /usr/etc/$file
-			# TODO: for some reason there is already aurora-fastfetch.sh and its trying to make it again
-			# https://github.com/noahdotpy/myublue/actions/runs/9842677770/job/27172121481#step:2:1856
 		done
 	done
 fi
