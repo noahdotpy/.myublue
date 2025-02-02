736c044aa7059bc026808e21cf06f63a0029f1eb
noahdotpy <noahdotpy@proton.me>
2025-02-02 14:27:19 +1000

    fix: use correct command to remove firefox

--- files/scripts/common-post/remove-firefox-if-exists.sh
+++ files/scripts/common-post/remove-firefox-if-exists.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 if [[ ! -f "/usr/bin/firefox" ]]; then
 	echo "Firefox doesn't exists, not removing..."
 else
-	rpm-ostree remove firefox
-	rpm-ostree remove firefox-langpacks
+	rpm-ostree override remove firefox
+	rpm-ostree override remove firefox-langpacks
 fi
