6d1ae0281afd08deb571cbed43e5d602dd4e4f7c
noahdotpy <noahdotpy@proton.me>
2025-02-05 16:39:44 +1000

    remove the firefox-langpacks in the same transaction as firefox

--- files/scripts/common-post/remove-firefox-if-exists.sh
+++ files/scripts/common-post/remove-firefox-if-exists.sh
@@ -5,6 +5,5 @@ set -euo pipefail
 if [[ ! -f "/usr/bin/firefox" ]]; then
 	echo "Firefox doesn't exists, not removing..."
 else
-	rpm-ostree override remove firefox
-	# rpm-ostree override remove firefox-langpacks
+	rpm-ostree override remove firefox firefox-langpacks
 fi
